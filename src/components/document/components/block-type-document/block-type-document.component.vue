2b58901aadfeb63ded602ca812b023414f0d39fd
sokolovsky <sokolovsky@alfa.im>
2022-10-12 15:42:42 +0700

    fix: correct use of src (2022-10-12)

--- src/components/document/components/block-type-document/block-type-document.component.vue
+++ src/components/document/components/block-type-document/block-type-document.component.vue
@@ -11,16 +11,16 @@
         <div class="lg:h-10"></div>
         <div class="mx-auto lg:flex px-8 gap-14 h-[442px]">
             <div 
-                v-for="id in store.getTypeDocument" :key="id" 
+                v-for="item in store.getTypeDocument" :key="item" 
                 class="flex flex-col w-1/3 bg-[#FFFFFF] border-solid border-t-[1px] border-[#E4ECF0] shadow-[0_24px_24px_rgba(0,72,109,0.2)]"
             >
                 <div class="mt-8 ml-8 mb-8 mr-8 w-full">
                     <div class="h-16">
-                        <img :src="`src/components/document/components/block-type-document/image/${ id.title }.png`"/>
+                        <img :src="`/images/${ item.title }.png`" alt="Illustration">
                     </div>
                     <div class="mt-20 flex flex-col justify-center">
                         <p class="text-[#00486D] font-serif lg:text-xl font-normal">
-                            {{ id.description }}
+                            {{ item.description }}
                         </p>
                         <p class="lg:w-[118px] mt-44 text-[#00486D] font-serif lg:text-xl font-normal px-3 py-2 rounded hover:bg-blue-200 hover:cursor-pointer">
                             {{ button }}
