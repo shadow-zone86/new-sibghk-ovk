d1ff4110befdd6c44d805bd9b6a79b415c79e53e
sokolovsky <sokolovsky@alfa.im>
2022-10-12 12:19:17 +0700

    fix: update block type document (2022-10-12)

--- src/store/document.ts
+++ src/store/document.ts
@@ -8,31 +8,27 @@ export const useDocumentStore = defineStore({
             title: 'Документы',
             description: 'Нормативно-правовые акты, устанавливающие статус, функции, права, обязанности и ответственность ОВК, а так же акты, которыми ОВК руководствуется в своей деятельности',
         } as Description,
-        activities: {
-            title: '',
-            description: 'Документы по деятельности ОВК'
-        } as Description,
-        responsibilities: {
-            title: '',
-            description: 'Документы по применению мер ответственности'
-        } as Description,
-        case: {
-            title: '',
-            description: 'Положение об ОВК'
-        } as Description
+        typeDocument: {
+            activities: {
+                title: '',
+                description: 'Документы по деятельности ОВК'
+            } as Description,
+            responsibilities: {
+                title: '',
+                description: 'Документы по применению мер ответственности'
+            } as Description,
+            case: {
+                title: '',
+                description: 'Положение об ОВК'
+            } as Description
+        } as any
     }),
     getters: {
         getDescription(state) {
             return state.description
         },
-        getTypeActivities(state) {
-            return state.activities
-        },
-        getTypeResponsibilities(state) {
-            return state.responsibilities
-        },
-        getTypeCase(state) {
-            return state.case
+        getTypeDocument(state) {
+            return state.typeDocument
         }
     },
     actions: {}
