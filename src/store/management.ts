e20fe44c9c1cd439b371f47430a14ec37295ce09
sokolovsky <sokolovsky@alfa.im>
2022-10-27 10:30:39 +0700

    feat: when send mail to head of department, put in copy of head of department (2022-10-27)

--- src/store/management.ts
+++ src/store/management.ts
@@ -8,7 +8,7 @@ export const useManagementStore = defineStore({
             title: 'Конев Павел Иванович',
             description: 'Заместитель генерального директора предприятия по внутреннему контролю',
             quote: 'Уважать всякого человека, как самого себя, и поступать с ним, как мы желаем, чтобы с нами поступали, — выше этого нет ничего.',
-            url: 'mailto:PaIKonev@sibghk.ru'
+            url: 'mailto:PaIKonev@sibghk.ru?cc=MiSDmitriev@sibghk.ru'
         } as Management,
     }),
     getters: {
